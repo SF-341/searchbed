707e4b6ba83439c4161d7e663ad8db1ca808a28f
wonyus <lucy_oliv@outlook.com>
2021-09-04 03:13:38 +0700

    update covid api

--- src/components/Covidapi.js
+++ src/components/Covidapi.js
@@ -7,6 +7,8 @@ import { makeStyles, withStyle } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Container } from '@material-ui/core/'
 
 
 
@@ -37,6 +39,42 @@ const Covidapi = () => {
       textAlign: 'center',
       color: theme.palette.text.secondary,
     },
+    boxPink: {
+      background: 'linear-gradient(45deg, #ee3fb7 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 80,
+      padding: '15px',
+    },
+    boxGreen: {
+      background: 'linear-gradient(45deg, #16b701 30%, #00fbff 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 80,
+      padding: '15px',
+    },
+    boxRed: {
+      background: 'linear-gradient(45deg, #ed4040 30%, #ff00bb 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 80,
+      padding: '15px',
+    },
+    boxBlue: {
+      background: 'linear-gradient(45deg, #3b63f1 30%, #1ce9e5 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 80,
+      padding: '10px',
+    },
   }));
 
 
@@ -50,71 +88,82 @@ const Covidapi = () => {
 
   return (
     <div>
-      <div container className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>
-              <Typography variant="h5" color="inherit">
-              New case
-            </Typography>
-              <Typography variant="h6" color="inherit">
-                {items.new_case}
-              </Typography>
-              <Typography variant="h8" color="inherit">
-                Total case
-              </Typography>
-              <Typography variant="h6" color="inherit">
-              {items.total_case}
-              </Typography></Paper>
+      <Container>
+        <div Container className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>
+                <Typography variant="h5" color="inherit">
+                  <Box className={classes.boxPink} bgcolor="primary.main" color="primary.contrastText">
+                    New case
+                  </Box>
+                </Typography>
+                <Typography variant="h6" color="inherit">
+                  {items.new_case}
+                </Typography>
+                <Typography variant="h8" color="inherit">
+                  Total case
+                </Typography>
+                <Typography variant="h6" color="inherit">
+                  {items.total_case}
+                </Typography></Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>
+                
+                  <Box className={classes.boxBlue} bgcolor="primary.main" color="primary.contrastText">
+                  <Typography variant="h5" color="inherit">New case excludeabroad</Typography>
+                    
+                  </Box>
+                
+                <Typography variant="h6" color="inherit">
+                  {items.new_case_excludeabroad}
+                </Typography>
+                <Typography variant="h8" color="inherit">
+                  Total case excludeabroad
+                </Typography>
+                <Typography variant="h6" color="inherit">
+                  {items.total_case_excludeabroad}
+                </Typography></Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>
+                <Typography variant="h5" color="inherit">
+                  <Box className={classes.boxRed} bgcolor="primary.main" color="primary.contrastText">
+                    New death
+                  </Box>
+                </Typography>
+                <Typography variant="h6" color="inherit">
+                  {items.new_death}
+                </Typography>
+                <Typography variant="h8" color="inherit">
+                  Total death
+                </Typography>
+                <Typography variant="h6" color="inherit">
+                  {items.total_case}
+                </Typography></Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>
+                <Typography variant="h5" color="inherit">
+                  <Box className={classes.boxGreen} bgcolor="primary.main" color="primary.contrastText">
+                    New recovered
+                  </Box>
+                </Typography>
+                <Typography variant="h6" color="inherit">
+                  {items.new_recovered}
+                </Typography>
+                <Typography variant="h8" color="inherit">
+                  Total recovered
+                </Typography>
+                <Typography variant="h6" color="inherit">
+                  {items.total_recovered}
+                </Typography>
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>
-              <Typography variant="h5" color="inherit">
-              New case excludeabroad
-            </Typography>
-            <Typography variant="h6" color="inherit">
-                {items.new_case_excludeabroad}
-              </Typography>
-              <Typography variant="h8" color="inherit">
-              Total case excludeabroad
-              </Typography>
-              <Typography variant="h6" color="inherit">
-                {items.total_case_excludeabroad}
-              </Typography></Paper>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>
-              <Typography variant="h5" color="inherit">
-              New death
-            </Typography>
-            <Typography variant="h6" color="inherit">
-                {items.new_death}
-              </Typography>
-              <Typography variant="h8" color="inherit">
-              Total death
-              </Typography>
-              <Typography variant="h6" color="inherit">
-                {items.total_case}
-              </Typography></Paper>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>
-              <Typography variant="h5" color="inherit">
-              New recovered
-              </Typography>
-              <Typography variant="h6" color="inherit">
-                {items.new_recovered}
-              </Typography>
-              <Typography variant="h8" color="inherit">
-              Total recovered
-              </Typography>
-              <Typography variant="h6" color="inherit">
-                {items.total_recovered}
-              </Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
+        </div>
+      </Container>
     </div>
   );
 };
