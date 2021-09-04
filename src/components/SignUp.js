9492c390f524482943fa3f8b04c2c30bacda98a4
wonyus <lucy_oliv@outlook.com>
2021-09-04 15:59:23 +0700

    sign up update

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -36,12 +36,24 @@ const SignUp = () => {
                 <h1>Sign Up</h1>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
+                        <label for="username" className="form-label">Username</label>
+                        <input type="text" name="username" className="form-control" id="username" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="name" className="form-label">Name</label>
+                        <input type="text" name="name" className="form-control" id="name" />
+                    </div>
+                    <div className="mb-3">
                         <label for="exampleInputEmail1" className="form-label">Email address</label>
                         <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
 
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" name="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Comfirm Password</label>
                         <input type="password" name="password" className="form-control" id="exampleInputPassword1" />
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -33,7 +33,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            CVD
+            CCTT
             <i class='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
