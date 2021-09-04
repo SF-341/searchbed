41a8bfdc321f82cc3868ed42c1fae42402275512
wonyus <lucy_oliv@outlook.com>
2021-09-04 16:35:03 +0700

    sign up update

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,11 +4,28 @@ import firebaseConfig from '../config'
 
 const SignUp = () => {
     const [currentUser, setCurrentUser] = useState(null);
+    const [ name, setName ] = useState("");
+    const [ username, setUsername ] = useState("");
+    const [ email, setEmail ] = useState("");
+
+    const handleChange = (e) => {
+        if (e.target.name === "name"){
+            setName(e.target.value)
+        }else if (e.target.name === "username"){
+            setUsername(e.target.value)
+        }else if (e.target.name === "email"){
+            setEmail(e.target.value)
+        }
+    }
+
+    
+    
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        
         const { email, password } = e.target.elements;
-
+        
         if (email.value == "" || password.value == "") {
             alert("email and password is required");
         } else {
@@ -37,24 +54,24 @@ const SignUp = () => {
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label for="username" className="form-label">Username</label>
-                        <input type="text" name="username" className="form-control" id="username" />
+                        <input type="text" name="username" className="form-control" id="username" onChange={handleChange}/>
                     </div>
                     <div className="mb-3">
                         <label for="name" className="form-label">Name</label>
-                        <input type="text" name="name" className="form-control" id="name" />
+                        <input type="text" name="name" className="form-control" id="name" onChange={handleChange}/>
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleChange}/>
 
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" name="password" className="form-control" id="exampleInputPassword1" />
+                        <input type="password" name="password" className="form-control" id="exampleInputPassword1"onChange={handleChange} />
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputPassword1" className="form-label">Comfirm Password</label>
-                        <input type="password" name="password" className="form-control" id="exampleInputPassword1" />
+                        <input type="password" name="password" className="form-control" id="exampleInputPassword1" onChange={handleChange}/>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
